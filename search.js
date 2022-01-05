b4e92f4bdeab66be6cdeef3fdf895f1116a1801e
krkode <lambdaghini@gmail.com>
2022-01-05 12:51:22 +1100

    removing urlsafeval + minor clean up

--- search.js
+++ search.js
@@ -9,42 +9,26 @@ function search(){
 }
 
 function generateUrl(){
-  let apiKey = getURLSafeVal($("#password"));
-  //let searchText =  getURLSafeVal($("#textSearch"));
-  let searchText = $("#textSearch").val();
+  let year = $("#selectYear").val();
+  let startdate = generateDate(year,0,1);
+  let enddate = generateDate(year,11,31);
 
   let url = new URL('https://www.googleapis.com/youtube/v3/search?')
 
   let params = {
                 part: 'snippet',
                 maxResults: 25,
-                q: searchText,
+                publishedAfter: startdate,
+                publishedBefore: enddate,
+                q: $("#textSearch").val(),
                 type: 'video',
-                key: apiKey
+                key: $("#password").val()
             }
   url.search = new URLSearchParams(params).toString();
-  url.href = AppendDateToUrl(url.href)
   return url;
-}
-
-//work around cause i think (not super sure) the google api doesn't support url encoding on ':' character, dates with '%3A' returns limited search results
-function AppendDateToUrl(href){
-  let year = $("#selectYear").val();
-  let startdate = generateDate(year,0,1);
-  let enddate = generateDate(year,11,31);
-
-  href = href + "&publishedAfter=" + startdate
-              + "&publishedBefore=" + enddate;
-  
-  return href;
-}
-
-function getURLSafeVal(element){
-  return encodeURIComponent(element.val());
 }
 
 function generateDate(year, month, day){
   let date = new Date(year,month,day).toISOString();
-  //date = encodeURIComponent(date);
   return date;
 }
